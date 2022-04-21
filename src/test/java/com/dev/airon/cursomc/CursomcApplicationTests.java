74313302ebe975a92af65f4aea4bb3df1b504212
devairon <airon_gm@hotmail.com>
2022-04-21 15:46:29 -0300

    first commit

---
+++ src/test/java/com/dev/airon/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.airon.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
